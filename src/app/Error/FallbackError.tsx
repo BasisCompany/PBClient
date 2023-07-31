d39398c1ddaad951b1deb9acda0db3aeb5462854
1Gvozd1 <62207561+1Gvozd1@users.noreply.github.com>
2023-07-31 19:01:23 +0300

    Refactor ErrorPage

--- src/app/Error/FallbackError.tsx
+++ src/app/Error/FallbackError.tsx
@@ -1,14 +1,88 @@
-// import { FC } from "react";
-
-import { ErrorPage } from "./ErrorPage";
+import { Box, Button, Typography } from "@mui/material";
+import { PromptBuyIcon } from "../../assets/PromptBuyIcon";
 
 export const FallbackError = () => {
+    const reloadPage = () => {
+        location.reload();
+    };
     return (
-        <>
-            <ErrorPage
-                status={"404"}
-                description={"Запрашиваемый ресурс не найден на сервере"}
-            />
-        </>
+        <Box
+            sx={{
+                bgcolor: "background.default",
+                color: "text.primary",
+                height: "100vh",
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center",
+                overflow: "hidden",
+            }}
+        >
+            <Box
+                sx={{
+                    display: "flex",
+                    width: "100vw",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography
+                    variant="h2"
+                    color="text.primary"
+                    sx={{
+                        fontSize: { xs: "8vw", md: "8vh" },
+                        overflow: "hidden",
+                        cursor: "default",
+                    }}
+                >
+                    Упс!
+                </Typography>
+                <Typography
+                    variant="h1"
+                    color="text.secondary"
+                    sx={{
+                        fontSize: { xs: "6vw", md: "6vh" },
+                        overflow: "hidden",
+                        cursor: "default",
+                    }}
+                >
+                    Что-то пошло не так!
+                </Typography>
+                <PromptBuyIcon
+                    sx={{
+                        fontSize: { xs: "50vw", md: "50vh" },
+                        height: { xs: "30vw", md: "30vh" },
+                    }}
+                />
+                <Button
+                    onClick={reloadPage}
+                    sx={{
+                        backgroundColor: "primary.main",
+                        width: { xs: "40vw", md: "40vh" },
+                        height: { xs: "10vw", md: "10vh" },
+                        textTransform: "none",
+                        borderRadius: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        color: "text.primary",
+                        "&:hover": {
+                            backgroundColor: "action.hover",
+                        },
+                    }}
+                >
+                    <Typography
+                        variant="h6"
+                        color="text.primary"
+                        sx={{
+                            fontSize: { xs: "3vw", md: "3vh" },
+                            overflow: "hidden",
+                            cursor: "pointer",
+                        }}
+                    >
+                        Вернуться на главную
+                    </Typography>
+                </Button>
+            </Box>
+        </Box>
     );
 };
