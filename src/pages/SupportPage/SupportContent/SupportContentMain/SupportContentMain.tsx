cdf0d162b33fac5eecc574287700728e0d2d5b70
Stormpero <staralex.const@gmail.com>
2023-08-17 13:58:38 +0300

    supp

--- src/pages/SupportPage/SupportContent/SupportContentMain/SupportContentMain.tsx
+++ src/pages/SupportPage/SupportContent/SupportContentMain/SupportContentMain.tsx
@@ -1,5 +1,33 @@
 import { Outlet } from "react-router";
 
+// TODO[Артём]:
+// const support = {
+//     common: [
+//         {
+//             id: 0,
+//             title: "3123",
+//             text: "32413",
+//         },
+//         {
+//             id: 0,
+//             title: "3123",
+//             text: "32413",
+//         },
+//     ],
+//     auth: [
+//         {
+//             id: 0,
+//             title: "3123",
+//             text: "32413",
+//         },
+//         {
+//             id: 0,
+//             title: "3123",
+//             text: "32413",
+//         },
+//     ],
+// };
+
 export const SupportContentMain = () => {
     return (
         <>
