b01cb063294ef843bb16cb00e647bc715832d1d8
Stormpero <staralex.const@gmail.com>
2024-01-14 17:03:21 +0300

    delete cypress

--- cypress/e2e/spec.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("template spec", () => {
-    it("passes", () => {
-        cy.visit("/profile");
-        cy.get("[aria-label='Тёмная тема']").click();
-        cy.visit("/login");
-        cy.get("[name='email']").type("2323");
-    });
-});
