ed25b318f7aaf189cd0192925281e3ec041877d8
Stormpero <staralex.const@gmail.com>
2024-06-07 23:53:07 +0300

    add forwardRef for LinkIconButton

--- src/shared/ui/Links/LinkIconButton.tsx
+++ src/shared/ui/Links/LinkIconButton.tsx
@@ -1,14 +1,14 @@
 import { IconButton, IconButtonProps } from "@mui/material";
-import { FC } from "react";
+import { forwardRef } from "react";
 import { LinkProps as RouterLinkProps } from "react-router-dom";
 import { LinkBehavior } from "../Route/LinkBehavior";
 
-export const LinkIconButton: FC<IconButtonProps & RouterLinkProps> = ({
-    children,
-    to,
-    ...props
-}) => (
-    <IconButton component={LinkBehavior} to={to} {...props}>
-        {children}
-    </IconButton>
+type LinkIconButtonProps = IconButtonProps & RouterLinkProps;
+
+export const LinkIconButton = forwardRef<HTMLElement, LinkIconButtonProps>(
+    ({ children, to, ...props }, ref) => (
+        <IconButton component={LinkBehavior} to={to} ref={ref} {...props}>
+            {children}
+        </IconButton>
+    )
 );
