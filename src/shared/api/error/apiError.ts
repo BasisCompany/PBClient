b6b545b81d7c89a77b41c858e31a28c8dc6e4c7e
Stormpero <staralex.const@gmail.com>
2024-06-18 12:10:19 +0300

    update userMessage error from server

--- src/shared/api/error/apiError.ts
+++ src/shared/api/error/apiError.ts
@@ -1,11 +1,16 @@
 export interface ApiError {
     status: number;
-    data: { statusCode: number; message: string; error: string };
+    data: {
+        statusCode: number;
+        message: string;
+        error: string;
+        userMessage: string;
+    };
 }
 
 const defaultError = "Произошла ошибка! Повторите попытку позже";
 
 export const getUserErrorMessage = (error: ApiError) => {
-    const errorMessage = error?.data?.error && error?.data?.message;
+    const errorMessage = error?.data?.userMessage;
     return errorMessage ?? defaultError;
 };
