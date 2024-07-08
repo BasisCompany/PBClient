8981ba7a7f352dda9f332a56a89624b77cb6f789
Stormpero <staralex.const@gmail.com>
2024-07-08 13:07:11 +0300

    add CommentEmpty

--- src/pages/ProfilePage/ProfileTabs/ProfileComments/CommentsEmpty.tsx
+++ src/pages/ProfilePage/ProfileTabs/ProfileComments/CommentsEmpty.tsx
@@ -1,26 +1,18 @@
 import { Box, Typography } from "@mui/material";
-import { CatIcon } from "../../../../assets/Cat";
+import CommentsDisabledIcon from "@mui/icons-material/CommentsDisabled";
+import { FlexBox } from "@/shared/ui/FlexBox";
 
 export const CommentsEmpty = () => (
-    <Box
+    <FlexBox
         sx={{
-            display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            minHeight: "300px",
+            minHeight: "250px",
         }}
     >
-        <Box>
-            {/*TODO[Артем]: Изменить картинку и прозрачность*/}
-            <CatIcon
-                sx={{
-                    fontSize: { xs: "50vw", md: "50vh" },
-                    height: { xs: "50vw", md: "50vh" },
-                }}
-            />
-            <Typography sx={{ textAlign: "center", mt: 2 }}>
-                Пока здесь нет комментариев
-            </Typography>
+        <Box textAlign="center">
+            <CommentsDisabledIcon sx={{ fontSize: 80, mb: 3 }} />
+            <Typography fontSize={18}>Пока здесь нет комментариев</Typography>
         </Box>
-    </Box>
+    </FlexBox>
 );
